f384b2568e55690c7f6fc202b38540c7b851064a
Marsel Gabbasov <marsel13_98@mail.ru>
2024-07-27 23:06:21 +0500

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ $(document).ready(function () {
     setTimeout(function () {
         $("header").addClass("in-viewport");
         $(".anim-style").addClass("in-viewport");
+        $(".products-line-sec").addClass("in-view");
+        $(".single-chamber-sec").addClass("in-view");
 
     }, 500);
 });
@@ -36,27 +38,62 @@ $(document).ready(function () {
 
 
 
-let $animation_element = $('.anim-newSing');
-let $window = $(window);
-function check_if_in_view() {
-    let window_height = $window.height();
-    let window_top_position = $window.scrollTop();
-    let window_bottom_position = ( window_height + window_top_position );
-    $.each($animation_element, function() {
-        let $element = $(this);
-        let element_height = $element.outerHeight();
-        let element_top_position = $element.offset().top;
-        let element_bottom_position = (element_height + element_top_position);
-        if((element_bottom_position >= window_top_position) &&
-            (element_top_position <= window_bottom_position)) {
-            $element.addClass('in-view');
-        }
-    });
+// let $animation_element = $('.anim-newSing');
+// let $window = $(window);
+// function check_if_in_view() {
+//     let window_height = $window.height();
+//     let window_top_position = $window.scrollTop();
+//     let window_bottom_position = ( window_height + window_top_position);
+//     $.each($animation_element, function() {
+//         let $element = $(this);
+//         let element_height = $element.outerHeight();
+//         let element_top_position = $element.offset().top;
+//         let element_bottom_position = (element_height + element_top_position);
+//         if((element_bottom_position >= window_top_position * .9) &&
+//             (element_top_position <= window_bottom_position * 1.1)) {
+//             $element.addClass('in-view');
+//         }
+//     });
+// }
+// $window.on('scroll resize', check_if_in_view);
+// $window.trigger('scroll');
+
+let animatedElements = document.querySelectorAll('.anim-newSing');
+
+let Visible = function(target) {
+    // Все позиции элемента
+    let targetPosition = {
+            top: window.pageYOffset + target.getBoundingClientRect().top,
+            left: window.pageXOffset + target.getBoundingClientRect().left,
+            right: window.pageXOffset + target.getBoundingClientRect().right,
+            bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+        },
+        // Получаем позиции окна
+        windowPosition = {
+            top: window.pageYOffset,
+            left: window.pageXOffset,
+            right: window.pageXOffset + document.documentElement.clientWidth,
+            bottom: window.pageYOffset + document.documentElement.clientHeight
+        };
+
+    if (targetPosition.bottom * 1.2 > windowPosition.top &&
+        targetPosition.top * .8 < windowPosition.bottom) {
+        target.classList.add("in-view")
+    } else {
+
+    }
+};
+
+window.addEventListener('scroll', function() {
+    initAnimElements()
+});
+initAnimElements()
+
+function initAnimElements() {
+    animatedElements.forEach(el => {
+        Visible(el)
+    })
 }
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
-
-
 
 
 
@@ -90,30 +127,44 @@ let generaSwiper = new Swiper(".generations-slider", {
     },
 });
 
+let generaSwiper2 = new Swiper(".generations-slider2", {
+    noSwiping: true,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    speed: 3000,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: true,
+    },
+});
+
 
 
 
 
 let completedSwiper = new Swiper(".completed-projects-slider", {
-    slidesPerView: 3,
+    slidesPerView: "auto",
     // loop: true,
     speed: 1000,
-    spaceBetween: 14,
+    // loopAdditionalSlides: 10,
+    spaceBetween: 0,
     breakpoints: {
-        '1299': {
-            slidesPerView: 3,
-            slidesPerGroup: 1,
-            spaceBetween: 14,
-        },
-        '760': {
-            slidesPerView: 2,
-            slidesPerGroup: 1,
-        },
-        '320': {
-            slidesPerView: 2,
-            slidesPerGroup: 1,
-            spaceBetween: 6,
-        },
+        // '1299': {
+        //     slidesPerView: 3,
+        //     slidesPerGroup: 1,
+        //     spaceBetween: 14,
+        // },
+        // '570': {
+        //     slidesPerView: 2,
+        //     slidesPerGroup: 1,
+        // },
+        // '320': {
+        //     slidesPerView: 1,
+        //     slidesPerGroup: 1,
+        //     spaceBetween: 6,
+        // },
     },
     navigation: {
         nextEl: ".completed-button-next",
@@ -148,35 +199,47 @@ let solutionsSwiper = new Swiper(".solutions__slider", {
         el: ".solutions-pagination",
         clickable: true,
     },
-});
-
-
-
-let glassSwiper = new Swiper(".this-glass-slider", {
-    slidesPerView: 2,
-    spaceBetween: 14,
-    loop: true,
-    speed: 1000,
     breakpoints: {
-        '1199': {
-            slidesPerView: 2,
+        '1024': {
+            slidesPerView: 3,
             slidesPerGroup: 1,
             spaceBetween: 14,
         },
-        '760': {
-            slidesPerView: 2,
-            slidesPerGroup: 1,
-        },
         '320': {
-            slidesPerView: 2,
-            slidesPerGroup: 1,
+            spaceBetween: 20,
         },
-    },
-    navigation: {
-        nextEl: ".this-glass-next",
-        prevEl: ".this-glass-prev",
     },
 });
+
+
+function glassSliders() {
+    let SlidersArray = []
+
+    let sliders = document.querySelectorAll(".this-glass-slider")
+    sliders.forEach((slider,index) => {
+        slider.setAttribute("data-slider-id",index)
+
+        SlidersArray[index] = new Swiper(`.this-glass-slider[data-slider-id="${index}"]`, {
+            slidesPerView: "auto",
+            spaceBetween: 0,
+            speed: 1000,
+
+            watchSlidesProgress: true,
+            preventClicks :true,
+            a11y: false,
+            observer: true,
+            observeParents: true,
+            observeSlideChildren: true,
+
+            navigation: {
+                nextEl: `.this-glass-slider[data-slider-id="${index}"] ~ .this-glass-next`,
+                prevEl: `.this-glass-slider[data-slider-id="${index}"] ~ .this-glass-prev`,
+            },
+        })
+    })
+}
+
+glassSliders()
 
 
 $(function () {
@@ -211,27 +274,10 @@ $(function () {
 
 
 let interestedSwiper = new Swiper(".interested-slider", {
-    slidesPerView: 4,
-    spaceBetween: 27,
-    loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 0,
+    // loop: true,
     speed: 1000,
-    breakpoints: {
-        '1199': {
-            slidesPerView: 4,
-            slidesPerGroup: 1,
-            spaceBetween: 27,
-        },
-        '760': {
-            slidesPerView: 3,
-            slidesPerGroup: 1,
-            spaceBetween: 20,
-        },
-        '320': {
-            slidesPerView: 2,
-            slidesPerGroup: 1,
-            spaceBetween: 20,
-        },
-    },
     navigation: {
         nextEl: ".interested-button-next",
         prevEl: ".interested-button-prev",
@@ -245,27 +291,10 @@ let interestedSwiper = new Swiper(".interested-slider", {
 
 
 let productiondSwiper = new Swiper(".production-slider", {
-    slidesPerView: 3,
-    spaceBetween: 12,
-    loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 0,
+    // loop: true,
     speed: 1000,
-    breakpoints: {
-        '1199': {
-            slidesPerView: 3,
-            slidesPerGroup: 1,
-            spaceBetween: 12,
-        },
-        '760': {
-            slidesPerView: 3,
-            slidesPerGroup: 1,
-            spaceBetween: 12,
-        },
-        '320': {
-            slidesPerView: 2,
-            slidesPerGroup: 1,
-            spaceBetween: 12,
-        },
-    },
     navigation: {
         nextEl: ".production-next",
         prevEl: ".production-prev",
@@ -277,6 +306,7 @@ let productiondSwiper = new Swiper(".production-slider", {
 
 let missionSwiper = new Swiper(".our-mission-slider", {
     slidesPerView: 1,
+    spaceBetween: 20,
     loop: true,
     speed: 1000,
     navigation: {
@@ -317,8 +347,30 @@ $('.close-filter').on('click',function () {
 
 $('.project-cnt-open').on('click', function (e) {
     e.stopPropagation();
+    if($(this).parent().hasClass('project-cnt-active')){
+        $('.project-cnt-box').removeClass('project-cnt-active');
+        $('.project-glass-sec').removeClass('_z-minus');
+        $('.architectural-wrap').removeClass('_z-minus');
+    }else{
+        $('.project-cnt-box').removeClass('project-cnt-active');
+        $(this).parent().addClass('project-cnt-active');
+        $('.project-glass-sec').addClass('_z-minus');
+        $('.architectural-wrap').addClass('_z-minus');
+    }
+});
+
+$('.project-cnt-click .apply').on('click', function (e) {
+    e.stopPropagation();
     $('.project-cnt-box').removeClass('project-cnt-active');
-    $(this).parent().toggleClass('project-cnt-active');
+    $('.project-glass-sec').removeClass('_z-minus');
+    $('.architectural-wrap').removeClass('_z-minus');
+});
+
+$('.project-cnt-click .closed').on('click', function (e) {
+    e.stopPropagation();
+    $('.project-cnt-box').removeClass('project-cnt-active');
+    $('.project-glass-sec').removeClass('_z-minus');
+    $('.architectural-wrap').removeClass('_z-minus');
 });
 
 
@@ -331,6 +383,8 @@ $(window).on('click', function (e) {
     let menuSort = $('.project-cnt-box');
     if (e.target !== menuSort) {
         menuSort.removeClass('project-cnt-active')
+        $('.project-glass-sec').removeClass('_z-minus');
+        $('.architectural-wrap').removeClass('_z-minus');
     }
 });
 
@@ -448,3 +502,112 @@ $(window).scroll(function () {
         a = 1;
     }
 });
+
+
+$("._tab").click(function() {
+    let tabId = $(this).attr("data-tab")
+    $(this).siblings("._tab").removeClass("_active")
+    $(this).addClass("_active")
+    $(".tab-content").removeClass("_active")
+    $(`.${tabId}`).addClass("_active")
+})
+
+function Sliders() {
+    let SlidersArray = []
+
+    let sliders = document.querySelectorAll(".slider")
+    sliders.forEach((slider,index) => {
+        slider.setAttribute("data-slider-id",index)
+
+        SlidersArray[index] = new Swiper(`.slider[data-slider-id="${index}"] .swiper-container`, {
+            slidesPerView: "auto",
+            spaceBetween: 0,
+            watchSlidesProgress: true,
+            preventClicks :true,
+            a11y: false,
+            observer: true,
+            observeParents: true,
+            observeSlideChildren: true,
+
+            navigation: {
+                nextEl: `.slider[data-slider-id="${index}"] .swiper-button-next`,
+                prevEl: `.slider[data-slider-id="${index}"] .swiper-button-prev`,
+            },
+            pagination: {
+                el: `.slider[data-slider-id="${index}"] .swiper-pagination`,
+                type: 'bullets',
+                clickable: true,
+            },
+        })
+    })
+}
+Sliders()
+
+let getScrollBarSize = (function() {
+    var el = window.document.createElement('textarea'), style = {
+        'visibility': 'hidden', 'position': 'absolute', 'zIndex': '-2147483647',
+        'top': '-1000px', 'left': '-1000px', 'width': '1000px', 'height': '1000px',
+        'overflow': 'scroll', 'margin': '0', 'border': '0', 'padding': '0'
+    }, support = el.clientWidth !== undefined && el.offsetWidth !== undefined;
+
+    for (var key in style) {
+        if (style.hasOwnProperty(key)) {
+            el.style[key] = style[key];
+        }
+    }
+
+    return function() {
+        var size = null;
+        if (support && window.document.body) {
+            window.document.body.appendChild(el);
+            size = [el.offsetWidth - el.clientWidth, el.offsetHeight - el.clientHeight];
+            window.document.body.removeChild(el);
+        }
+
+        document.documentElement.style.setProperty('--scrollWidth', `${size[0]}px`);
+        return size;
+    };
+})();
+
+window.addEventListener('resize', () => {
+    getScrollBarSize()
+})
+
+getScrollBarSize()
+
+function fix100vh() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+$(".this-glass-photo,.completed-projects-pic").click(function () {
+    $(".gallery-popup").addClass("_active")
+    $("body").addClass("fixed-body2")
+})
+
+
+$("._close-gallery-popup").click(function () {
+    $(".gallery-popup").removeClass("_active")
+    $("body").removeClass("fixed-body2")
+})
+
+let progressFillItems = document.querySelectorAll(".progress__fill")
+progressFillItems.forEach(progressFill => {
+    if(progressFill){
+        let pathLength = progressFill.getTotalLength();
+        let fillPercent = +progressFill.getAttribute("data-fill");
+        progressFill.style.strokeDasharray = pathLength + ' ' + pathLength;
+        progressFill.style.strokeDashoffset = pathLength * -1;
+
+        setTimeout(()=>{
+            setProgressFill(progressFill,fillPercent)
+        },1000)
+    }
+})
+
+
+function setProgressFill(el,progressNum) {
+    let pathLength = el.getTotalLength();
+    let drawLength = pathLength * (progressNum / 100);
+    el.style.strokeDashoffset = drawLength - pathLength
+}
