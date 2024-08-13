b6d8eb7ec32fe6a3813dc197cfec3edbbfa9baf9
Marsel Gabbasov <marsel13_98@mail.ru>
2024-08-13 12:31:48 +0500

    fixed bugs

--- assets/js/12-08.js
+++ assets/js/12-08.js
@@ -1,3 +1,16 @@
+window.addEventListener('DOMContentLoaded', () => {
+    fix100vh();
+    window.addEventListener('resize', () => {
+        fix100vh();
+    })
+})
+
+function fix100vh() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+
 $(".spoiler-card__show").click(function () {
     let parent = $(this).parents(".spoiler-card")
     $(parent).toggleClass("_open")
@@ -33,3 +46,41 @@ $(".materials-filter__clear").click(function () {
         $(el).trigger("change");
     })
 })
+
+let body = document.querySelector("body")
+let modalTogglers = document.querySelectorAll("[data-modal]")
+let modals = document.querySelectorAll(".modal")
+
+modalTogglers.forEach(el => {
+    el.onclick = function (e) {
+        e.preventDefault()
+        let modalId = el.getAttribute("data-modal")
+        openModal(modalId)
+    }
+})
+
+function closeModals() {
+    modals.forEach(modal => {
+        modal.classList.remove("_active")
+        body.classList.remove("fixed-body2")
+    })
+}
+
+function openModal(modalId) {
+    closeModals()
+    let modal = document.querySelector(`#${modalId}`)
+    let closeButtons = modal.querySelectorAll(`._close-modal`)
+
+    modal.classList.add("_active")
+    body.classList.add("fixed-body2")
+
+
+    closeButtons.forEach(btn => {
+        btn.onclick = ()=> {
+            modal.classList.remove("_active")
+            body.classList.remove("fixed-body2")
+        }
+    })
+}
+
+
