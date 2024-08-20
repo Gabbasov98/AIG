327958d07ce4f1875a24c4305685ec58faa6cc52
Marsel Gabbasov <marsel13_98@mail.ru>
2024-08-20 14:10:57 +0500

    fixed bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -93,9 +93,13 @@ let Visible = function(target) {
             right: window.pageXOffset + document.documentElement.clientWidth,
             bottom: window.pageYOffset + document.documentElement.clientHeight
         };
-
-    if (targetPosition.bottom - 40 > windowPosition.top &&
-        targetPosition.top + 40 < windowPosition.bottom) {
+    console.log(targetPosition.bottom)
+    console.log(windowPosition.top)
+    console.log(targetPosition.top)
+    console.log(windowPosition.bottom)
+    console.log("__")
+    if (targetPosition.bottom - 10 > windowPosition.top &&
+        targetPosition.top - 30 < windowPosition.bottom) {
         target.classList.add("in-view")
     } else {
 
